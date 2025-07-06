f3ac2a728a9cce93a2d76423341113604bbad603
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2025-07-06 21:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	var freq = {};
+
+  // Count frequencies using basic for loop
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    if (freq[char] === undefined) {
+      freq[char] = 1;
+    } else {
+      freq[char]++;
+    }
+  }
+
+  // Find first non-repeated character
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    if (freq[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
